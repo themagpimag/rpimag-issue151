ab48f93ddd62b43748c0edee7103b18eae174018
robthez <robertzwetsloot@gmail.com>
2025-02-26 16:52:25 +0000

    Updated code

---
+++ GetStartedWithSenseHATV2_part1/magic8ball.py
@@ -0,0 +1,24 @@
+import random
+import time
+from sense_hat import SenseHat
+
+sh = SenseHat()
+
+sh.show_message("Ask a question?")
+
+time.sleep(3)
+
+replies = [
+
+'Signs point to yes',
+'Without a doubt',
+'You may rely on it',
+'Do not count on it',
+'Looking good',
+'Cannot predict now',
+'It is decidedly so',
+'Outlook not so good'
+
+]
+
+sh.show_message(random.choice(replies))
---
+++ GetStartedWithSenseHATV2_part1/show_message.py
@@ -0,0 +1,4 @@
+from sense_hat import SenseHat
+sense = SenseHat()
+
+sense.show_letter("P", text_colour=[255, 255, 0], back_colour=[0, 0, 255])
